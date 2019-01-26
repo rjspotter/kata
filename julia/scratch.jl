a621a204bf5a4db23870a04328b0f1a490a41218
R. Potter <rjspotter@gmail.com>
2019-01-26 20:57:35 +0000

    the tree isn't precise enough

--- julia/scratch.jl
+++ julia/scratch.jl
@@ -7,11 +7,13 @@ using CSV
 using DataFrames
 using StatsBase
 using Statistics
+using ASTInterpreter2
+using DebuggerFramework
 
 const global dataframe = open("/home/ubuntu/data/kc_house_data/kc_house_data.csv") |> CSV.File |> DataFrame
 
 const global label = :price
-const global threashold = 2500
+const global threashold = 500
 const global max_branches = 10
 
 function partition(feature_values)
@@ -54,12 +56,17 @@ function infogain(df, feature)
   baseline = entropy(df[label])
   values = df[feature] |> partition
   entropy_diff = values .|> entropy_with_value .|> y -> baseline - y
-  sum(entropy_diff) / length(values)
+  l = length(values)
+  if l < 1
+    0
+  else
+    sum(entropy_diff) / l
+  end
 end
 
 function gainz(df, ignore)
   acc = Dict()
-  Threads.@threads for n in names(df)
+  for n in names(df)
     if n == label || n in ignore
       #nop
     else
@@ -83,35 +90,50 @@ end
 
 function build_tree(df, ignore=[], depth=0)
   max_depth = (df |> names |> size |> first) - (ignore |> size |> first) - 1
+  println("==================================")
+  println(depth)
+  println(maxdepth)
+  println("==================================")
   if (std(df[label]) < threashold) || depth >= max_depth
     df[label]
   else
     feature = next_feature(gainz(df, ignore), ignore)
     new_nodes = Dict()
-    Threads.@threads for v in partition(df[feature])
-      new_nodes[v] = build_tree(
-        filter(x -> inpartition(v, x[feature]), df),
-        cat(ignore, feature, dims=1),
-        depth + 1
-      )
+    for v in partition(df[feature])
+      subframe = filter(x -> inpartition(v, x[feature]), df)
+      new_ignore = cat(ignore, feature, dims=1)
+      subtree = build_tree(subframe, new_ignore, (depth + 1))
+      # println("===========================")
+      # println(depth)
+      # println(feature)
+      # println(v)
+      # println(names(subframe))
+      # println(new_ignore)
+      # println("===========================")
+      new_nodes[v] = subtree
     end
     Dict(feature => new_nodes)
   end
 end
-
-# tree = build_tree(dataframe, [:id, :sqft_lot15, :sqft_living15, :date]) && true
 
 function treewalk(item, branch::Array, value=0)
   branch
 end
 
 function treewalk(item, branch::Dict, value=0)
-  ks = tree |> keys |> collect
-  if ks |> size |> first == 1
-    key = ks |> first
-    treewalk(item, branch[key], (item[key] |> first))
+  ks = (branch |> keys |> collect)
+  if (ks |> size |> first) == 1
+    ky = ks |> first
+    new_branch = branch[ky]
+    treewalk(item, new_branch, (item[ky] |> first))
   else
-    filter(x -> inpartition(value, x), ks)
+    ky = filter(x -> inpartition(x, value), ks) |> first
+    new_branch = branch[ky]
+    treewalk(item, new_branch)
   end
 end
 
+
+# tree = build_tree(dataframe, [:id, :sqft_lot15, :sqft_living15, :date])
+# df = filter(x -> x[:id] == 9834201367, dataframe)
+# rng = treewalk(df, tree)
