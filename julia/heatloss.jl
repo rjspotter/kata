7ee000d2552f7fbc85bbd3b8ae036aec5b2578d9
R. Potter <rjspotter@gmail.com>
2019-05-31 01:26:34 +0000

    starting on a first generation, so I can do GA optimization

--- julia/heatloss.jl
+++ julia/heatloss.jl
@@ -1,13 +1,15 @@
 cavity_u = 1 / 12.5 # 11(insulation) 1(cladding) 0.5(drywall)
 stud_u = 1 / 5 # 3.5(stud) 1(cladding) 0.5(drywall)
 window_u = 1 / 3.35
+door_u = 1 / 5
+bay_u = 1 / 20 # 19.25(5.5in@3.5r insulation) 0.5(drywall)
 
 spacing = 16
 stud = 1.5
 header = 13.5 # 12(above) 1.5(below)
 plate = 4.5 # total plate inches 2 top 1 bottom
 
-function wall_assembly(width, height=96, windows=[])
+function wall_assembly(width, height=96, windows=[], doors=[])
   total_area = width * height
   window_area = 0
 
@@ -20,13 +22,34 @@ function wall_assembly(width, height=96, windows=[])
     stud_area += (header * w[1]) + (2 * stud * stud_height)
   end
 
-  cavity_area = (total_area - stud_area) - window_area
+  for d in doors
+    door_area += foldl(*, d)
+    stud_area += (header * d[1]) + (2 * stud * stud_height)
+  end
+
+  cavity_area = ((total_area - stud_area) - window_area) - door_area
 
   stud_area_percent = stud_area / total_area
   cavity_area_percent = cavity_area / total_area
   window_area_percent = window_area / total_area
+  door_area_percent = door_area / total_area
 
-  (stud_u * stud_area_percent) + (cavity_u * cavity_area_percent) + (window_u * window_area_percent)
+  (stud_u * stud_area_percent) + (cavity_u * cavity_area_percent) + (window_u * window_area_percent) + (door_u * door_area_percent) 
+end
+
+function ceiling_assembly(width, height)
+  total_area = width * height
+
+  stud_count = ceil(width / spacing)
+  stud_height = height - plate
+  stud_area = (stud * stud_height * stud_count) / total_area
+
+  cavity_area = total_area - stud_area
+
+  stud_area_percent = stud_area / total_area
+  cavity_area_percent = cavity_area / total_area
+
+  (stud_u * stud_area_percent) + (bay_u * cavity_area_percent)
 end
 
 
@@ -39,18 +62,18 @@ radiant_loss += wall_assembly(173, 96, [[53, 62]])
 radiant_loss += wall_assembly(131, 96, [[33, 39]])
 
 # Laundry Room
-radiant_loss += wall_assembly(58, 96) # add door
+radiant_loss += wall_assembly(58, 96, [], [[32, 80]])
 radiant_loss += wall_assembly(58, 96)
 radiant_loss += wall_assembly(126, 96, [[53, 51]])
 
 # Den
-radiant_loss += wall_assembly(286, 96, [[78, 62]]) # add door
+radiant_loss += wall_assembly(286, 96, [[78, 62]], [[78, 80]])
 
 # Parlor
 radiant_loss += wall_assembly(199, 96, [[87, 75]])
 
 # Foyer
-radiant_loss += wall_assembly(80, 96) # add door
+radiant_loss += wall_assembly(80, 96, [], [[36, 80]])
 
 # Em Office
 radiant_loss += wall_assembly(120, 96, [[68.5, 73]])
@@ -61,11 +84,15 @@ radiant_loss += wall_assembly(95, 96, [[28, 38]])
 # Master Bedroom
 radiant_loss += wall_assembly(144, 96, [[75, 80]])
 
+# Master Bathroom
+radiant_loss += wall_assembly(78, 123, [[21, 25]])
+radiant_loss += wall_assembly(122, 123, [[33, 76]])
+
 # Closet
 radiant_loss += wall_assembly(60, 96)
-radiant_loss += wall_assembly(51, 123)
-radiant_loss += wall_assembly(83, 123, [[24, 28]])
+radiant_loss += wall_assembly(51, 103, [[21, 25]])
+radiant_loss += wall_assembly(124, 103, [[24, 28]])
 
 # My Office
-radiant_loss += wall_assembly(120, 96)
-radiant_loss += wall_assembly(108, 96)
+radiant_loss += wall_assembly(120, 96, [[59, 73.5]])
+radiant_loss += wall_assembly(165, 96, [[41.5, 49.5]])
--- julia/scratch.jl
+++ julia/scratch.jl
@@ -24,57 +24,7 @@ end
 
 const global label = :SalePrice
 
-# if the spread of values is large break them into pieces sized by the std deviation
-function large_partition(feature_values)
-  acc = []
-  step = feature_values |> std
-  max = feature_values |> maximum
-  fst = 0
-  snd = (feature_values |> minimum) + step
-  while snd < (max - 1)
-    proposed = (first = fst, second = snd)
-    if (filter(x -> inpartition(proposed, x), feature_values) |> size |> first) > 0
-      push!(acc, proposed)
-      fst = snd
-    end
-    snd = snd + step
-  end
-  proposed = (first = fst, second = Inf32)
-  push!(acc, proposed)
-  acc
-end
 
-# if the spread is small just break by unique values (but include down to 0 and up to infinity)
-function small_partition(feature_values)
-  feature_values = sort(feature_values)
-  acc = []
-  fst = 0
-  for i in unique(feature_values)
-    snd = i
-    proposed = (first = fst, second = snd)
-    if (filter(x -> inpartition(proposed, x), feature_values) |> size |> first) > 0
-      push!(acc, proposed)
-      fst = snd
-    end
-  end
-  proposed = (first = fst, second = Inf32)
-  push!(acc, proposed)
-  acc
-end
-
-# if the values are straings just partition by value
-function partition(feature_values::Array{Union{Missing, String}})
-  map(x -> (first = x, second = x), feature_values)
-end
-
-# what kind of (numeric) partition should we do
-function partition(feature_values)
-  if (feature_values |> unique |> length) > 15
-    large_partition(feature_values)
-  else
-    small_partition(feature_values)
-  end
-end
 
 function inpartition(tup, val::Union{String, Char})
   (tup.first == val)
@@ -84,76 +34,143 @@ function inpartition(tup, val)
   (val >= tup.first && val < tup.second)
 end
 
-function entropy(data)
-  len = data |> length |> float
-  function item_entropy(category)
-    ratio = float(category) / len
-    -1 * ratio * log2(ratio)
-  end
-  data |> countmap |> values |> v -> map(item_entropy, v) |> sum
-end
+function generate_forrest(full_set, (probability, number, threashold, partition_size))
 
-function infogain(df, feature)
-  function entropy_with_value(val)
-    entropy(filter(x -> inpartition(val, x[feature]), df)[label])
+  # if the spread of values is large break them into pieces sized by the std deviation
+  function large_partition(feature_values)
+    acc = []
+    step = feature_values |> std
+    max = feature_values |> maximum
+    fst = 0
+    snd = (feature_values |> minimum) + step
+    while snd < (max - 1)
+      proposed = (first = fst, second = snd)
+      if (filter(x -> inpartition(proposed, x), feature_values) |> size |> first) > 0
+        push!(acc, proposed)
+        fst = snd
+      end
+      snd = snd + step
+    end
+    proposed = (first = fst, second = Inf32)
+    push!(acc, proposed)
+    acc
   end
-  baseline = entropy(df[label])
-  values = df[feature] |> partition
-  entropy_diff = values .|> entropy_with_value .|> y -> baseline - y
-  l = length(values)
-  if l < 1
-    0
-  else
-    sum(entropy_diff) / l
-  end
-end
 
-function gainz(df)
-  acc = Dict()
-  for n in names(df)
-    if n == label
-      #nop
+  # if the spread is small just break by unique values (but include down to 0 and up to infinity)
+  function small_partition(feature_values)
+    feature_values = sort(feature_values)
+    acc = []
+    fst = 0
+    for i in unique(feature_values)
+      snd = i
+      proposed = (first = fst, second = snd)
+      if (filter(x -> inpartition(proposed, x), feature_values) |> size |> first) > 0
+        push!(acc, proposed)
+        fst = snd
+      end
+    end
+    proposed = (first = fst, second = Inf32)
+    push!(acc, proposed)
+    acc
+  end
+
+  # if the values are straings just partition by value
+  function partition(feature_values::Array{Union{Missing, String}})
+    map(x -> (first = x, second = x), feature_values)
+  end
+
+  # what kind of (numeric) partition should we do
+  function partition(feature_values)
+    if (feature_values |> unique |> length) > partition_size
+      large_partition(feature_values)
     else
-      acc[n] = infogain(df, n)
+      small_partition(feature_values)
     end
   end
-  acc
-end
 
-function next_feature(feature_entropies)
-  function largest(a, b)
-    if b.first == label
-      a
+  function entropy(data)
+    len = data |> length |> float
+    function item_entropy(category)
+      ratio = float(category) / len
+      -1 * ratio * log2(ratio)
+    end
+    data |> countmap |> values |> v -> map(item_entropy, v) |> sum
+  end
+
+  function infogain(df, feature)
+    function entropy_with_value(val)
+      entropy(filter(x -> inpartition(val, x[feature]), df)[label])
+    end
+    baseline = entropy(df[label])
+    values = df[feature] |> partition
+    entropy_diff = values .|> entropy_with_value .|> y -> baseline - y
+    l = length(values)
+    if l < 1
+      0
     else
-      a.second > b.second ? a : b
+      sum(entropy_diff) / l
     end
   end
-  candidate = reduce(largest, feature_entropies)
-  candidate.first
-end
 
-function small_difference(df, threashold)
-  min = (df[label] |> minimum)
-  max = (df[label] |> maximum)
-  (max - min) < threashold
-end
-
-function build_tree(df, threashold)
-  if (df |> names |> size |> first) < 2 || small_difference(df, threashold)
-    df[label]
-  else
-    feature = df |> gainz |> next_feature
-    new_nodes = Dict()
-    for v in partition(df[feature])
-      subframe = filter(x -> inpartition(v, x[feature]), df)
-      deletecols!(subframe, feature)
-      subtree = build_tree(subframe, threashold)
-      new_nodes[v] = subtree
+  function gainz(df)
+    acc = Dict()
+    for n in names(df)
+      if n == label
+        #nop
+      else
+        acc[n] = infogain(df, n)
+      end
     end
-    Dict(feature => new_nodes)
+    acc
   end
+
+  function next_feature(feature_entropies)
+    function largest(a, b)
+      if b.first == label
+        a
+      else
+        a.second > b.second ? a : b
+      end
+    end
+    candidate = reduce(largest, feature_entropies)
+    candidate.first
+  end
+
+  function small_difference(df)
+    min = (df[label] |> minimum)
+    max = (df[label] |> maximum)
+    (max - min) < threashold
+  end
+
+  function build_tree(df)
+    if (df |> names |> size |> first) < 2 || small_difference(df)
+      df[label]
+    else
+      feature = df |> gainz |> next_feature
+      new_nodes = Dict()
+      for v in partition(df[feature])
+        subframe = filter(x -> inpartition(v, x[feature]), df)
+        deletecols!(subframe, feature)
+        subtree = build_tree(subframe)
+        new_nodes[v] = subtree
+      end
+      Dict(feature => new_nodes)
+    end
+  end
+
+  function random_tree_selection()
+    acc = []
+    for n in 1:number
+      tmp = filter(row -> (rand(1:100) < probability), full_set)
+      push!(acc, build_tree(tmp))
+    end
+    acc
+  end
+
+  random_tree_selection()
 end
 
+# Walk the generated trees
 function treewalk(item, branch::Union, value=0)
   sum(branch) / length(branch)
 end
@@ -186,49 +203,10 @@ function treewalk(item, branch::Dict, value)
   treewalk(item, new_branch)
 end
 
-function many_tree_slices(full_set, slices)
-  @assert slices > 1
-
-  acc = []
-  max = size(full_set, 1)
-  @assert max > 0
-  @assert typeof(max) == Int64
-
-  slice_size = trunc(Int64, ceil(max / slices))
-  @assert slice_size > 0
-  @assert typeof(slice_size) == Int64
-  lower = 1
-  upper = slice_size * 2
-
-  while lower < max
-    subset = full_set[lower:upper, :]
-    tree = build_tree(subset, 1000)
-    mn = subset[label] |> minimum
-    mx = subset[label] |> maximum
-    push!(acc, (tree = tree, min = mn, max = mx))
-    lower = lower + slice_size
-    upper = upper + slice_size
-    if upper > max
-      upper = max
-    end
-    @assert upper <= max
-  end
-  acc
-end
-
-function random_tree_selection(full_set, probability, number)
-  acc = []
-  for n in 1:number
-    tmp = filter(row -> (rand(1:100) < probability), full_set)
-    push!(acc, build_tree(tmp, 1000))
-  end
-  acc
-end
-
 test_set = copy(dataframe[1:292, :])
 train_set = copy(dataframe[293:1460, :])
 
-trees = random_tree_selection(train_set, 50, 10);
+# trees = generate_forrest(train_set, 50, 10, 1000, 15);
 
 function test_trees(trees)
   myerr = []
@@ -261,9 +239,26 @@ function test_trees(trees)
     # end
   end
   println(missed_cnt)
-  println(sqrt(mean(myerr.^2.)))
+  # println(sqrt(mean(myerr.^2.)))
+  sqrt(mean(myerr.^2.))
 end
 
-test_trees(trees)
+generation = Dict()
+for i in 1:5
+  println(i)
+  probability = rand(1:100)
+  number = rand(1:25)
+  threashold = rand(1:10000)
+  partition_size = rand(5:35)
+  args = (probability, number, threashold, partition_size)
+  trees = generate_forrest(train_set, args);
+  generation[test_trees(trees)] = args
+end
 
-# 0.26138826805475596
+# Dict{Any,Any} with 5 entries:
+#   0.329141 => (75, 3, 9762, 17)
+#   0.299757 => (12, 5, 9771, 25)
+#   0.26881  => (72, 18, 4933, 29)
+#   0.269774 => (65, 5, 7249, 11)
+#   0.255723 => (27, 25, 8344, 13)
+
